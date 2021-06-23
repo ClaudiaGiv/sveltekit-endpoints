dd1e04d64dc5066fde8bb0845f718d7862e6a9b4
ClaudiaGiv <clau_givan@yahoo.com>
2021-06-23 22:37:45 +0300

    Add netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,12 +15,27 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		vite: {
+			resolve: {
+				dedupe: ['svelte', 'urql']
+			},
+			optimizeDeps: {
+				exclude: ['@urql/svelte', '@urql/exchange-request-policy', '@urql/devtools'],
+				// exclude: Object.keys(pkg.dependencies || {}).filter((d) => !['graphql'].includes(d)),
+				include: ['graphql']
+			},
+			ssr: {
+				// Until https://github.com/vitejs/vite/issues/2579
+				// noExternal: Object.keys(pkg.dependencies || {})
+			}
+		},
+		adapter: adapter()
+		// adapter: adapter({
+		// 	// default options are shown
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: null
+		// })
 	}
 };
 
